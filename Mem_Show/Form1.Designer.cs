01d8cdadd1dace0ac1c530287bf6c5497c779135
Liu Ying <kilosnow@live.com>
2019-12-17 22:25:29 +0800

    1

---
+++ Mem_Show/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Mem_Show
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.notifyIconMS = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStripMS = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tsmiTheme = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiBoot = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsmiExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.timerMS = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStripMS.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIconMS
+            // 
+            this.notifyIconMS.ContextMenuStrip = this.contextMenuStripMS;
+            this.notifyIconMS.Text = "可用内存";
+            this.notifyIconMS.Visible = true;
+            this.notifyIconMS.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIconMS_MouseDoubleClick);
+            // 
+            // contextMenuStripMS
+            // 
+            this.contextMenuStripMS.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiTheme,
+            this.tsmiBoot,
+            this.toolStripSeparator1,
+            this.tsmiExit});
+            this.contextMenuStripMS.Name = "contextMenuStripMS";
+            this.contextMenuStripMS.Size = new System.Drawing.Size(101, 76);
+            // 
+            // tsmiTheme
+            // 
+            this.tsmiTheme.Checked = true;
+            this.tsmiTheme.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.tsmiTheme.Name = "tsmiTheme";
+            this.tsmiTheme.Size = new System.Drawing.Size(100, 22);
+            this.tsmiTheme.Text = "暗夜";
+            this.tsmiTheme.Click += new System.EventHandler(this.tsmiTheme_Click);
+            // 
+            // tsmiBoot
+            // 
+            this.tsmiBoot.Name = "tsmiBoot";
+            this.tsmiBoot.Size = new System.Drawing.Size(100, 22);
+            this.tsmiBoot.Text = "自启";
+            this.tsmiBoot.Click += new System.EventHandler(this.tsmiBoot_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(97, 6);
+            // 
+            // tsmiExit
+            // 
+            this.tsmiExit.Name = "tsmiExit";
+            this.tsmiExit.Size = new System.Drawing.Size(100, 22);
+            this.tsmiExit.Text = "退出";
+            this.tsmiExit.Click += new System.EventHandler(this.tsmiExit_Click);
+            // 
+            // timerMS
+            // 
+            this.timerMS.Enabled = true;
+            this.timerMS.Interval = 1000;
+            this.timerMS.Tick += new System.EventHandler(this.timerMS_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label1.Location = new System.Drawing.Point(24, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 35);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "奇迹银巧";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Violet;
+            this.ClientSize = new System.Drawing.Size(212, 149);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "内存显示";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.formMain_Load);
+            this.contextMenuStripMS.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIconMS;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStripMS;
+        private System.Windows.Forms.ToolStripMenuItem tsmiExit;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.Timer timerMS;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem tsmiTheme;
+        private System.Windows.Forms.ToolStripMenuItem tsmiBoot;
+    }
+}
+
---
+++ Mem_Show/Form1.cs
@@ -0,0 +1,183 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Mem_Show
+{
+    public partial class FormMain : Form
+    {
+        // 引用
+        [DllImport("kernel32")]
+        private static extern void GlobalMemoryStatus(ref MEMORY_INFO meminfo);
+        //变量
+        MEMORY_INFO mInfo = new MEMORY_INFO();
+        readonly Rectangle RECT = new Rectangle(0, 0, 16, 16);
+
+        Pen pen = new Pen(Color.White, 1);
+        SolidBrush brush = new SolidBrush(Color.White);
+        Font font = new Font("微软雅黑", 8);
+        Bitmap bmp;
+        Graphics g;
+        readonly string strAppName = "MemShow";
+        string memNum = "99";
+
+        public struct MEMORY_INFO
+        {
+            public uint dwLength;
+            public uint dwMemoryLoad;
+            public uint dwTotalPhys;
+            public uint dwAvailPhys;
+            public uint dwTotalPageFile;
+            public uint dwAvailPageFile;
+            public uint dwTotalVirtual;
+            public uint dwAvailVirtual;
+        }
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        // APP 初始化 & 加载
+        private void formMain_Load(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(RECT.Width, RECT.Height);
+            g = Graphics.FromImage(bmp);
+            g.Clear(Color.FromArgb(0, 0, 0, 0));
+            g.ResetClip();
+
+            if (isAutoBoot())
+                tsmiBoot.Checked = true;
+            else
+                tsmiBoot.Checked = false;
+
+            this.ShowInTaskbar = false;
+            notifyIconMS.Visible = true;
+        }
+
+        // 双击 弹出关于
+        private void notifyIconMS_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show(
+                "本作品由：人贱人爱花贱花开的三个臭皮匠，联合制作！",
+                "关于 内存显示 v1.3.2020.1",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+
+        // 右键菜单 主题
+        private void tsmiTheme_Click(object sender, EventArgs e)
+        {
+            Color color;
+
+            if (tsmiTheme.Checked)
+            {
+                color = Color.Black;
+                tsmiTheme.Checked = false;
+            }
+            else
+            {
+                color = Color.White;
+                tsmiTheme.Checked = true;
+            }
+            pen = new Pen(color, 1);
+            brush = new SolidBrush(color);
+        }
+
+        // 右键菜单 自启
+        private void tsmiBoot_Click(object sender, EventArgs e)
+        {
+            if (tsmiBoot.Checked)
+            {
+                setAutoBoot(true);
+                tsmiBoot.Checked = false;
+            }
+            else
+            {
+                setAutoBoot(false);
+                tsmiBoot.Checked = true;
+            }
+        }
+
+        // 右键菜单 退出
+        private void tsmiExit_Click(object sender, EventArgs e)
+        {
+            pen = null;
+            brush = null;
+            font = null;
+            bmp = null;
+            g = null;
+
+            Application.Exit();
+        }
+
+        // 主时钟
+        private void timerMS_Tick(object sender, EventArgs e)
+        {
+            getMemInfo();
+            drawBmp();
+
+            Icon icon = Icon.FromHandle(bmp.GetHicon());
+
+            notifyIconMS.Icon = icon;
+            notifyIconMS.Text = "内存使用: " + memNum + "%";
+        }
+
+        // 检测是否为开机启动
+        private bool isAutoBoot()
+        {
+            try
+            {
+                string path = Application.ExecutablePath;
+                RegistryKey reg = Registry.CurrentUser;
+                RegistryKey run = reg.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run");
+                object key = run.GetValue(strAppName);
+                run.Close();
+                reg.Close();
+
+                if (null == key || !path.Equals(key.ToString()))
+                {
+                    return false;
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex);
+            }
+        }
+        //设置或取消开机启动,isAutoBoot=true为开机启动。
+        private void setAutoBoot(bool isAutoBoot)
+        {
+            string path = Application.ExecutablePath;
+            RegistryKey reg = Registry.CurrentUser;
+            RegistryKey run = reg.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run");
+
+            if (isAutoBoot)
+                run.DeleteValue(strAppName, false);
+            else
+                run.SetValue(strAppName, path);
+
+            run.Close();
+            reg.Close();
+        }
+
+        // 得到内存信息
+        private void getMemInfo()
+        {
+            GlobalMemoryStatus(ref mInfo);
+            memNum = mInfo.dwMemoryLoad.ToString();
+        }
+
+        // 画图
+        private void drawBmp()
+        {
+            g.Clear(Color.FromArgb(0, 0, 0, 0));
+            g.ResetClip();
+            g.DrawRectangle(pen, 0, 0, 15, 15);
+            g.DrawString(memNum, font, brush, 0, 0);
+        }
+    }
+}
